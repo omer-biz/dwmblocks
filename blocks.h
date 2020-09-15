8fcaceae9b3a01419bfdc5fd7b6fd70c5ef0eca0
omer <omerabdi@protonmail.com>
2020-09-15 22:30:41 +0300

    meh

--- blocks.h
+++ blocks.h
@@ -2,8 +2,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/		/*Update Signal*/
-	{"  ", 		EXEC("time.sh"), 		60, 					0},
 	{"",		EXEC("volume.sh"),		0,						23},
+	{"  ", 	EXEC("time.sh"), 		60, 					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
